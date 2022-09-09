ba0d4eddec55ea8f3c703243b84c097135dd881a
Salih DEMİROĞ <salihdemirog@gmail.com>
2022-09-09 11:53:36 +0300

    Login işlemi implemente edildi.

--- SanalMarket.Web/Controllers/AccountController.cs
+++ SanalMarket.Web/Controllers/AccountController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using SanalMarket.Infrastructure.Abstract;
+using System.Security.Claims;
 
 namespace SanalMarket.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -11,9 +22,32 @@ namespace SanalMarket.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Login(string email, string password, bool remember)
+        public async Task<IActionResult> LoginAsync(string email, string password, bool remember)
         {
-            return null;
+            var user =_userService.Login(email,password);
+            if(user == null)
+            {
+                ViewBag.Message = "Kullanıcı adı veya şifre hatalı";
+                return View();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Mail),
+                new Claim(ClaimTypes.Email, user.Mail),
+                new Claim(ClaimTypes.GivenName, $"{user.FirstName} {user.LastName}")
+            };
+
+            var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimPrinciple = new ClaimsPrincipal(claimIdentity);
+
+            await HttpContext.SignInAsync(claimPrinciple,new AuthenticationProperties
+            {
+                IsPersistent = remember,
+            });
+
+            return RedirectToAction("Index", "Product");
         }
     }
 }
